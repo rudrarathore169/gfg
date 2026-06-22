cc258cc675de6d9a6d7c293d34336c993d0c24db
Rudra Pratap Singh Rathore <152763842+rudrarathore169@users.noreply.github.com>
2026-06-22 10:06:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
+++ Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
@@ -1,23 +1,26 @@
 class Solution {
   public:
-    int solve(int n, int k, vector<int>&dp){
-        if(n==1){
-            return k;
-        }
-        if(n==2){
-            return k+k*(k-1);
+    int solve(int n, int k){
+        if (n == 1) return k;
+        if (n == 2) return k + k * (k - 1);
+
+         
+         
+        int prev2 = k;
+        int prev1  =k+k*(k-1);
+        
+        for(int i=3;i<=n;i++){
+        int ans=prev2*(k-1)+prev1*(k-1);
+        prev2 = prev1;
+        prev1 = ans;
         }
         
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        dp[n]=solve(n-2,k,dp)*(k-1)+solve(n-1,k,dp)*(k-1);
         
-        return dp[n];
+        return prev1;
     }
     int countWays(int n, int k) {
         // code here
-        vector<int>dp(n+1,-1);
-        return solve(n,k,dp);
+        
+        return solve(n,k);
     }
 };
